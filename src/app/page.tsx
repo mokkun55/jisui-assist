1fad87103af94e9417ab8cad3f04105f9d0c8d06
もっくん <mokkunpc@gmail.com>
2025-05-02 13:06:27 +0900

    feat: ホーム画面の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
+import { LinkButton } from "~/components/buttons/linkButton";
 import { BaseLayout } from "~/components/layouts/baseLayout";
 
 export default function Home() {
   return (
-    <div>
-      <BaseLayout>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <p className="text-lg">
-          This is a sample Next.js app with Tailwind CSS.
-        </p>
-        <p className="text-lg">You can start building your app from here.</p>
-        <p className="text-lg">Happy coding!</p>
-      </BaseLayout>
-    </div>
+    <BaseLayout>
+      <div className="p-[16px]">
+        <h1 className="text-3xl font-bold text-center">ホーム画面</h1>
+        <div className="flex flex-col gap-4 mt-8 px-8">
+          <LinkButton label="ご飯を記録する" url="/photo" />
+          <LinkButton label="記録の履歴" url="/photo/history" />
+          <LinkButton label="買い物リスト" url="/shopping" />
+          {/* ログインしてないときだけ表示するようにする */}
+          <LinkButton label="ログイン" url="/login" />
+          {/* ログアウトは、リンク先でuseEffectでログアウトする */}
+          <LinkButton label="ログアウト" url="/logout" />
+        </div>
+      </div>
+    </BaseLayout>
   );
 }
